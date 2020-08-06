f2b426ad279ba084dc34e36b3721cde32cd49b6c
Jonathan Cavalcante <jonathancmpc@gmail.com>
2020-08-06 10:52:59 -0300

    Projeto expo criado

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>Hello World!! ❤</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#333333',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  title: {
+    textAlign: 'center',
+    color: '#FFF',
+    fontSize: 32,
+    fontWeight: 'bold',
+    transform: [
+      {rotateZ: '30deg'}
+    ]
+  }
 });
