59f5b5218d7ab0e7d1d752ed6bfa7f2bb71d624f
Jonathan Cavalcante <jonathancmpc@gmail.com>
2020-08-06 15:53:18 -0300

    Estilizando Tabs(sem icone)

--- src/routes/StudyTabs.tsx
+++ src/routes/StudyTabs.tsx
@@ -9,7 +9,41 @@ const { Navigator, Screen } = createBottomTabNavigator();
 function StudyTabs() {
   return (
     /* Não temos aqui o NavigationContainer pq só iremos mostrar uma vez as tabs */
-    <Navigator>
+    <Navigator
+      /* Estilizando as Tabs */
+      tabBarOptions={{
+        /* Container das abas */
+        style: {
+          elevation: 0, /* Tira a sombra no Android */
+          shadowOpacity: 0, /* Tira a sombra no IOS */
+          height: 64,
+        },
+        /* Estilização de cada aba */
+        tabStyle: {
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center'
+        },
+        /* Estilizando os ícones */
+        iconStyle: {
+          flex: 0,
+          width: 20,
+          height: 20,
+        },
+        /* Estilização do texto */
+        labelStyle: {
+          fontFamily: 'Archivo_700Bold',
+          fontSize: 13,
+          marginLeft: 16,
+        },
+        /* Cor da aba quando ela não está ativa e quando está */
+        inactiveBackgroundColor: '#fafafc',
+        activeBackgroundColor: '#ebebf5',
+        /* Cor do texto quando a aba não está ativa e quando está */
+        inactiveTintColor: '#c1bccc',
+        activeTintColor: '#32268d',
+      }}
+    >
       <Screen name="TeacherList" component={TeacherList} />
       <Screen name="Favorites" component={Favorites} />
     </Navigator>
