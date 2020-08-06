bb429b5dcafa0dbde72aedd77c303a1428c5331b
Jonathan Cavalcante <jonathancmpc@gmail.com>
2020-08-06 15:19:18 -0300

    Estilizando e finalizando pagina Dar Aulas

--- src/pages/GiveClasses/index.tsx
+++ src/pages/GiveClasses/index.tsx
@@ -1,10 +1,40 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ImageBackground } from 'react-native';
+/* Importando pacote para utilizarmos um botão mais bonito */
+import { RectButton } from 'react-native-gesture-handler';
+
+import giveClassesBgImage from '../../assets/images/give-classes-background.png';
+
+import styles from './styles';
+import { useNavigation } from '@react-navigation/native';
 
 function GiveClasses() {
+  const { goBack } = useNavigation();
+
+  /* Função para retornar a página anterior quando for clicado no botão 'Tudo Bem' */
+  function handleNavigateBack() {
+    goBack();
+  }
+
   return (
-    <View>
-      <Text>Teste</Text>
+    <View style={styles.container}>
+      {/* Todo Imagem de background é obrigatório passar o tamanho que irá ocupar na tela */}
+    <ImageBackground 
+      resizeMode='contain' 
+      source={giveClassesBgImage} 
+      style={styles.content}
+    >
+      <Text style={styles.title}>Quer ser um Proffy?</Text>
+      <Text style={styles.description}>
+        Para começar, você precisa se cadastrar como professor na nossa plataforma web.
+      </Text>
+
+    </ImageBackground>
+
+    <RectButton onPress={handleNavigateBack} style={styles.button}>
+      <Text style={styles.buttonText}>Tudo Bem</Text>
+    </RectButton>
+
     </View>
   )
 }
---
+++ src/pages/GiveClasses/styles.ts
@@ -0,0 +1,50 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#8257E5',
+    flex: 1,
+    justifyContent: 'center',
+    padding: 40,
+  },
+
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+
+  title: {
+    fontFamily: 'Archivo_700Bold',
+    color: '#FFF',
+    fontSize: 32,
+    lineHeight: 37,
+    maxWidth: 180,
+  },
+
+  description: {
+    marginTop: 24,
+    color: '#d4c2ff',
+    fontSize: 16,
+    lineHeight: 26,
+    fontFamily: 'Poppins_400Regular',
+    maxWidth: 240,
+  },
+
+  button: {
+    marginVertical: 40,
+    height: 58,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#04d361'
+  },
+
+  buttonText: {
+    color: '#FFF',
+    fontSize: 16,
+    fontFamily: 'Archivo_700Bold'
+  },
+
+});
+
+export default styles;
