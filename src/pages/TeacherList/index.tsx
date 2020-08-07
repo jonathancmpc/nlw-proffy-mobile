ead8451aa46b10d9306f862a062100e5e43805d4
Jonathan Cavalcante <jonathancmpc@gmail.com>
2020-08-07 12:48:52 -0300

    Criando scrow da lista e estilizando

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -5,13 +5,27 @@ import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
 
 import styles from './styles';
+import { ScrollView } from 'react-native-gesture-handler';
 
 function TeacherList() {
   return (
     <View style={styles.container}>
       <PageHeader title="Proffys disponíveis"/>
 
-      <TeacherItem />
+      {/* Criando Scrow da lista e estilizando ela */}
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16,
+        }}
+      >
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </ScrollView>
     </View>
   );
 }
