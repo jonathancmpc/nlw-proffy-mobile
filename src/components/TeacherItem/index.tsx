827412d6a33a6ef3265da9726de1b2494f640821
Jonathan Cavalcante <jonathancmpc@gmail.com>
2020-08-07 22:21:08 -0300

    Fazendo filtro buscando na API e listando

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,36 +1,51 @@
 import React from 'react';
 import { View, Image, Text } from 'react-native';
 
-import styles from './styles';
 import { RectButton } from 'react-native-gesture-handler';
 
 import heartOutlineIcon from '../../assets/images/icons/heart-outline.png'
 import unfavoriteIcon from '../../assets/images/icons/unfavorite.png'
 import whatsappIcon from '../../assets/images/icons/whatsapp.png'
 
-const TeacherItem: React.FC = () => {
+import styles from './styles';
+
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: number;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
   return (
     <View style={styles.container}>
       <View style={styles.profile}>
         <Image 
           style={styles.avatar}
-          source={{ uri: 'https://avatars1.githubusercontent.com/u/38424834?s=460&u=30562206e41cd265195a7af9c9d57eb6e6374ea8&v=4' }}
+          source={{ uri: teacher.avatar }}
         />
 
         <View style={styles.profileInfo}>
-          <Text style={styles.name}>Jonathan Cavalcante</Text>
-          <Text style={styles.subject}>Química</Text>
+          <Text style={styles.name}>{teacher.name}</Text>
+          <Text style={styles.subject}>{teacher.subject}</Text>
         </View>
       </View>
 
       <Text style={styles.bio}>
-        Esta é a bio...blablalbalbalblalbalbalblablalbalblablalbalblalbalbalbl!
+        {teacher.bio}
       </Text>
 
       <View style={styles.footer}>
         <Text style={styles.price}>
           Preço/hora {'  '}
-          <Text style={styles.priceValue}>R$ 20,00</Text>
+          <Text style={styles.priceValue}>R$ {teacher.cost}</Text>
         </Text>
 
         <View style={styles.buttonsContainer}>
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -3,14 +3,35 @@ import { View, Text, TextInput } from 'react-native';
 import { ScrollView, BorderlessButton, RectButton } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons';
 
+import api from '../../services/api';
+
 import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
 
 import styles from './styles';
 
 function TeacherList() {
   const [filtersVisible, setFiltersVisible] = useState(false);
+  const [teachers, setTeachers] = useState([]);
 
+  const [subject, setSubject] = useState('');
+  const [week_day, setWeekDay] = useState('');
+  const [time, setTime] = useState('');
+
+  async function handleFiltersSubmit() {
+    const response = await api.get('classes', {
+      params: {
+        subject,
+        week_day,
+        time
+      }
+    });
+
+    /* Fechando os filtros após a pesquisa */
+    setFiltersVisible(false);
+    setTeachers(response.data);
+  }
+  
   /* Função para setar verdadeiro ou false para visualização dos filtros */
   function handleToggleFiltersVisible() {
     setFiltersVisible(!filtersVisible);
@@ -32,6 +53,9 @@ function TeacherList() {
             <Text style={styles.label}>Matéria</Text>
             <TextInput 
               style={styles.input}
+              value={subject} 
+              /* Diferente da web, aqui nós pegamos o texto diretamente pelo onChangeText */
+              onChangeText={text => setSubject(text)}
               placeholder="Qual a matéria?"
               placeholderTextColor='#c1bccc'
             />
@@ -41,6 +65,9 @@ function TeacherList() {
                 <Text style={styles.label}>Dia da semana</Text>
                 <TextInput 
                   style={styles.input}
+                  value={week_day} 
+                  /* Diferente da web, aqui nós pegamos o texto diretamente pelo onChangeText */
+                  onChangeText={text => setWeekDay(text)}
                   placeholder="Qual o dia da semana?"
                   placeholderTextColor='#c1bccc'
                 />
@@ -50,13 +77,16 @@ function TeacherList() {
                 <Text style={styles.label}>Horário</Text>
                 <TextInput 
                   style={styles.input}
+                  value={time} 
+                  /* Diferente da web, aqui nós pegamos o texto diretamente pelo onChangeText */
+                  onChangeText={text => setTime(text)}
                   placeholder="Qual o Horário?"
                   placeholderTextColor='#c1bccc'
                 />
               </View>
             </View>
 
-            <RectButton style={styles.submitButton}>
+            <RectButton onPress={handleFiltersSubmit} style={styles.submitButton}>
               <Text style={styles.submitButtonText}>Filtrar</Text>
             </RectButton>
           </View>
@@ -71,11 +101,11 @@ function TeacherList() {
           paddingBottom: 16,
         }}
       >
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
+
+        {teachers.map((teacher: Teacher) => 
+          <TeacherItem key={teacher.id} teacher={teacher} />
+        )}
+
       </ScrollView>
     </View>
   );
