76160cb43f447682a2c134cadeb45da2fc2ed7d1
Jonathan Cavalcante <jonathancmpc@gmail.com>
2020-08-08 11:12:50 -0300

    Implementando logica botao favorito

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Image, Text, Linking } from 'react-native';
-
 import { RectButton } from 'react-native-gesture-handler';
+import AsyncStorage from '@react-native-community/async-storage';
 
 import heartOutlineIcon from '../../assets/images/icons/heart-outline.png'
 import unfavoriteIcon from '../../assets/images/icons/unfavorite.png'
@@ -21,12 +21,50 @@ export interface Teacher {
 
 interface TeacherItemProps {
   teacher: Teacher;
+  favorited: boolean;
 }
 
-const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher, favorited }) => {
+  /* Sempre que uma informação tiver que ser manipulada pelo usuário, ela precisa estar no estado */
+  const [isFavorited, setIsFavorited] = useState(favorited);
+  
   function handleLinkToWhatsapp() {
     /* Utilizando o deep linkin do whatsapp para abri-lo, toda aplicação deve ter um deep linkia para app's externos conectar com eles */
     Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`);
+  }
+
+  async function handleToggleFavorite() {
+    
+    const favorites = await AsyncStorage.getItem('favorites');
+
+    /* Como pode ter alguém que não tenha favoritos, temos que fazer essa condição */
+    let favoritesArray = [];
+    if (favorites) {
+      favoritesArray = JSON.parse(favorites);
+    }
+    
+    if (isFavorited) {
+      /* Procurando a posição deste professor dentro dos favoritos, item por item */
+      const favoriteIndex = favoritesArray.findIndex((teacherItem: Teacher) => {
+        return teacherItem.id === teacher.id;
+      });
+
+      /* Removendo o professor de dentro do array de favoritos. O splice encontra o indice dentro do array no primeiro parâmetro e no segundo é informado quantas posições quer remover a partir daquele índice */
+      favoritesArray.splice(favoriteIndex, 1);
+
+      /* Informando que este array não é favorito */
+      setIsFavorited(true);
+      
+    } else {
+      /* Incluindo um novo favorito no array */
+      favoritesArray.push(teacher);
+
+      /* Informando que este array são os favoritos */
+      setIsFavorited(true);
+    }
+
+    /* Inserindo o Array no banco de dados Storage(tranformando em string) */
+    await AsyncStorage.setItem('favorites', JSON.stringify(favoritesArray));
   }
 
 
@@ -55,9 +93,20 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
         </Text>
 
         <View style={styles.buttonsContainer}>
-          <RectButton style={[styles.favoriteButton, styles.favorited]}>
-            {/* <Image source={heartOutlineIcon} /> */}
-            <Image source={unfavoriteIcon} />
+          {/* Se estiver favoritado, utiliza o estilo dos favoritos e mostra o ícone dos favoritos, se não, mostra um estilo vazio e mostra o ícone do não favoritado */}
+          <RectButton
+            onPress={handleToggleFavorite}
+            style={[
+              styles.favoriteButton, 
+              isFavorited ? styles.favorited : {},
+            ]}
+          >
+
+            { isFavorited  
+              ? <Image source={unfavoriteIcon} /> 
+              : <Image source={heartOutlineIcon} />
+            }
+            
           </RectButton>
           
           <RectButton onPress={handleLinkToWhatsapp} style={styles.contactButton}>
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { View, Text, TextInput } from 'react-native';
 import { ScrollView, BorderlessButton, RectButton } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons';
+/* Importando o expo async storage para conseguirmos salvar os favoritos no storage do celular */
+import AsyncStorage from '@react-native-community/async-storage'
 
 import api from '../../services/api';
 
@@ -12,13 +14,34 @@ import styles from './styles';
 
 function TeacherList() {
   const [filtersVisible, setFiltersVisible] = useState(false);
+  const [favorites, setFavorites] = useState<number[]>([]);
   const [teachers, setTeachers] = useState([]);
 
   const [subject, setSubject] = useState('');
   const [week_day, setWeekDay] = useState('');
   const [time, setTime] = useState('');
 
+  function loadFavorites() {
+    /* Salvando os favoritos no storage. Este é o retorno do nosso banco de dados do storage(não é o banco relacional que fizemos a API), estou indo ao banco e procurando pela chace chamada favorites */
+    /* Este banco de dados só recebe texto, por isso se for uma lista tem que convertê-la em texto(JSON) */
+    AsyncStorage.getItem('favorites').then(response => {
+      if (response) {
+        /* Convertendo de volta para lista(ele vem como texto do storage) */
+        const favoritedTeachers = JSON.parse(response);
+        /* Percorrendo e pegando apenas o id do teacher */
+        const favoritedTeachersIds = favoritedTeachers.map((teacher: Teacher) => {
+          return teacher.id;
+        })
+
+        setFavorites(favoritedTeachersIds);
+      }
+    });
+  }
+
+  /* Faz os filtros e também carrega os favoritos */
   async function handleFiltersSubmit() {
+    loadFavorites();
+
     const response = await api.get('classes', {
       params: {
         subject,
@@ -102,9 +125,16 @@ function TeacherList() {
         }}
       >
 
-        {teachers.map((teacher: Teacher) => 
-          <TeacherItem key={teacher.id} teacher={teacher} />
-        )}
+        {teachers.map((teacher: Teacher) => {
+          return (
+            <TeacherItem 
+              key={teacher.id} 
+              teacher={teacher}
+              /* Verificando se o id do professor está incluido na lista de ids de favoritos */
+              favorited={favorites.includes(teacher.id)}
+            />
+          )
+        })}
 
       </ScrollView>
     </View>
